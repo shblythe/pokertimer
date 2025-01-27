be58b41edde5aba4b2cfc666c96245eb63d325cd
Stephen Blythe <stephen@urwick.co.uk>
2025-01-27 21:55:30 +0000

    Added nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    };
+    outputs = {self, nixpkgs}:
+        let
+            system = "x86_64-linux";
+            pkgs = nixpkgs.legacyPackages.${system};
+        in {
+            devShells.${system}.default = pkgs.mkShell {
+                name = "task-dev";
+                buildInputs = with pkgs; [
+                    nodejs_20
+                ];
+                # shellHook = ''
+                #     # bash to run when you enter the shell
+                #     ls
+                # '';
+            };
+        };
+}
